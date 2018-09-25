2d9b045549b81f7a223ca9ed78ba95afe215b0e0
Valerio Mario Casale <valerio.casale@gmail.com>
2018-09-26 00:42:05 +0200

    datasrouce refactoring

--- src/main/java/io/vforge/cauldron/config/CauldronDatasourcePrimary.java
+++ src/main/java/io/vforge/cauldron/config/CauldronDatasourcePrimary.java
@@ -2,20 +2,17 @@ package io.vforge.cauldron.config;
 
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
+
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 
 
-import javax.persistence.EntityManagerFactory;
+
 import javax.sql.DataSource;
 
 @EnableJpaRepositories(basePackages = "io.vforge.cauldron.repository.primary",
@@ -25,14 +22,7 @@ import javax.sql.DataSource;
 public class CauldronDatasourcePrimary extends AbstractCaluldronDatasource {
 
     @Primary
-    @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Primary
-    @Bean(name = "entityManagerFactory")
+    @Bean(name = "updatedEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean
     updatedEntityManagerFactory(EntityManagerFactoryBuilder builder,
                                 @Qualifier("dataSource") DataSource dataSource) {
@@ -41,13 +31,6 @@ public class CauldronDatasourcePrimary extends AbstractCaluldronDatasource {
                 .packages("io.vforge.cauldron.model.primary")
                 .properties(buildJpaProperties())
                 .build();
-    }
-
-    @Primary
-    @Bean(name = "transactionManager")
-    public PlatformTransactionManager transactionManager(
-            @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
     }
 
 }
